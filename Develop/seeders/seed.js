bc143a1d12c50d873faf698b66d784b1b2e52ac8
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-18 11:23:48 -0800

    updated seeders

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,47 +1,147 @@
-const { Workout } = require('../models')
+let mongoose = require("mongoose");
+let db = require("../models");
 
-const planSeed = [
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+let workoutSeed = [
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 1)),
-    tasks: [
+    day: new Date().setDate(new Date().getDate()-10),
+    exercises: [
       {
-        name: 'Take out trash',
-        duration: 5
-      },
-      {
-        name: 'Do laundry',
-        duration: 10
+        type: "resistance",
+        name: "Bicep Curl",
+        duration: 20,
+        weight: 100,
+        reps: 10,
+        sets: 4
       }
     ]
   },
   {
-    day: new Date(),
-    tasks: [
+    day: new Date().setDate(new Date().getDate()-9),
+    exercises: [
       {
-        name: 'Cook dinner',
-        duration: 60
-      },
+        type: "resistance",
+        name: "Lateral Pull",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-8),
+    exercises: [
       {
-        name: 'Walk the dog',
-        duration: 30
+        type: "resistance",
+        name: "Push Press",
+        duration: 25,
+        weight: 185,
+        reps: 8,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-7),
+    exercises: [
+      {
+        type: "cardio",
+        name: "Running",
+        duration: 25,
+        distance: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-6),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 20,
+        weight: 285,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-5),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-4),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Quad Press",
+        duration: 30,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-3),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-2),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Military Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-1),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench",
+        duration: 30,
+        distance: 2
       }
     ]
   }
-]
+];
 
-require('mongoose').connect('mongodb://localhost/workouts_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => {
-    
-    Plan.deleteMany({})
-      .then(() => Plan.insertMany(planSeed))
-      .then(() => {
-        console.log('records inserted!')
-        process.exit()
-      })
-      .catch(err => console.log(err))
-
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
   })
-  .catch(err => console.log(err))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
