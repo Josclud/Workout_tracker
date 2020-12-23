08b33e7a3542f2f3a06b8cbf583bde94788ff23f
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-23 12:16:06 -0800

    final code update

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 
 router.use('/api', require('./workoutRoutes.js'))
 
-router.use('', require('./htmlRoutes.js'))
+router.use('/', require('./htmlRoutes.js'))
 
 module.exports = router
